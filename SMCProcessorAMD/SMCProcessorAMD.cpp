d81b3fa5db963f70b28ad5dd1518ab82cf47b0c9
zhuhecun <2063809513@qq.com>
2024-05-16 17:18:28 +0800

    更新

--- SMCProcessorAMD/SMCProcessorAMD.cpp
+++ SMCProcessorAMD/SMCProcessorAMD.cpp
@@ -57,11 +57,6 @@ bool SMCProcessorAMD::setupKeysVsmc(){
     auto isdigit = [](auto l) { return l >= '0' && l <= '8'; };
     bool isMob = !strncmp(model, "MacBook", strlen("MacBook"));
 
-    // 只有第一个核心是真正的CPU温度，其它的全是频率(GHz *10，比如3.3Ghz显示的是33）
-    for(int core = 0; core <= this->totalNumberOfPhysicalCores; core++){
-
-            VirtualSMCAPI::addKey(KeyTCxC(core), vsmcPlugin.data, VirtualSMCAPI::valueWithSp(0, SmcKeyTypeSp78, new ClockCore(this, core)));
-   }
 
     // 核显温度
     VirtualSMCAPI::addKey(KeyTGDD, vsmcPlugin.data, VirtualSMCAPI::valueWithSp(0, SmcKeyTypeSp78, new TempCore(this, 0)));
